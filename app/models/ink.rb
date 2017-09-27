5c6c79e5e904a93a74f63dd6f670d54cbe5dc274
jonathan <jonathanyi81@gmail.com>
2017-09-27 14:20:34 -0400

    added users assocation to Ink model

--- app/models/ink.rb
+++ app/models/ink.rb
@@ -1,6 +1,7 @@
 class Ink < ActiveRecord::Base
   has_many :pens
   has_many :collections, through: :pens
+  has_many :users, through: :collections
 
   include Slugifiable::InstanceMethod
   extend Slugifiable::ClassMethod
