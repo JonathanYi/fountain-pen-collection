7a17cc9cc3d942bd696ccb1fefa7f235cae6800e
jonathan <jonathanyi81@gmail.com>
2017-09-27 13:35:30 -0400

    finished the remaining migrations

--- db/migrate/20170927171143_create_pens.rb
+++ db/migrate/20170927171143_create_pens.rb
@@ -1,4 +1,10 @@
 class CreatePens < ActiveRecord::Migration[5.1]
   def change
+    create_table :pens do |t|
+      t.string :name
+      t.string :brand
+      t.integer :collection_id
+      t.integer :ink_ud
+    end
   end
 end
--- db/migrate/20170927171211_create_inks.rb
+++ db/migrate/20170927171211_create_inks.rb
@@ -1,4 +1,8 @@
 class CreateInks < ActiveRecord::Migration[5.1]
   def change
+    create_table :inks do |t|
+      t.string :name
+      t.string :brand
+    end
   end
 end
