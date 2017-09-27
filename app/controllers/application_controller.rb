c0e56270cce19b269de08c94d3849051da8222ea
jonathan <jonathanyi81@gmail.com>
2017-09-27 14:58:56 -0400

    added view folders and layout and index page. created get root to display to login signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,13 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/vies'
+    set :views, 'app/views'
     enable :sessions
     set :session_secrete, "secret"
+  end
+
+  get '/' do
+    erb :"index"
   end
 
   helpers do
