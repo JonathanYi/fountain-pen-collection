914f43c1a754541fcb0850739136b08372bbbd18
Jonathan <jonathanyi81@gmail.com>
2017-09-30 12:40:18 -0400

    removed pry's and test route

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -1,6 +1,5 @@
 class CollectionController < ApplicationController
   get '/collections' do
-    binding.pry
     if logged_in?
       @collections = current_user.collections
       erb :"collections/collections"
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,21 +4,12 @@ class UserController < ApplicationController
   end
 
   post '/login' do
-    #binding.pry
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      binding.pry
-      redirect "/collectionsTest"
+
+      redirect "/collections"
     end
     #erb :"/collections"
-  end
-
-  get '/collectionsTest' do
-    binding.pry
-    #if logged_in?
-      @collections = current_user.collections
-      erb :"collections/collections"
-    #end
   end
 end
