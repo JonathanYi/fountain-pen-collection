9753e4b4e348e09796885cd50a78e798e7c13bae
jonathan <jonathanyi81@gmail.com>
2017-10-02 00:31:57 -0400

    added flash messaging for errors. Completed signup routes and view. new user was created

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,23 @@
+require 'rack-flash'
 class UserController < ApplicationController
+  use Rack::Flash
 
   get '/signup' do
     if logged_in?
       redirect "/collections"
     else
-      erb :"/users/create_usr.erb"
+      erb :"/users/create_user"
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:user_id] = @user.id
+      erb :"users/show"
+    else
+      flash[:message] = "Please Try Again. All fields should be filled out."
+      redirect "/signup"
     end
   end
 
