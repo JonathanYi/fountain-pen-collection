715ff1302c3f0073ffca6b82a156cf8685b405a4
jonathan <jonathanyi81@gmail.com>
2017-10-01 22:29:25 -0400

    changed layout, user show erb pages and routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,36 @@
 class UserController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/collections"
+    else
+      erb :"/users/create_usr.erb"
+    end
+  end
+
   get '/login' do
-    erb :"users/login"
+    if logged_in?
+      redirect "/collections"
+    else
+      erb :"users/login"
+    end
   end
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
+    #username or password was blank
+    if params[:username] == "" || params[:password] == ""
+      redirect "/login"
+    elsif @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-
-      redirect "/collections"
+      erb :"/users/show"
+    else
+      #username and/or oassword invalid
     end
-    #erb :"/collections"
+  end
+
+  get '/logout' do
+    session.clear
+    redirect "/login"
   end
 end
