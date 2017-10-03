886dd7b635da7c25a8864527f4db4b28ff61a781
jonathan <jonathanyi81@gmail.com>
2017-10-02 22:59:13 -0400

    added route for creating new collection, created post route, empty. Created initial view for creating new collection. Will add checkboxes for available pens

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -6,8 +6,21 @@ class CollectionController < ApplicationController
     end
   end
 
+  post '/collections' do
+
+  end
+
+  get '/collections/new' do
+    if logged_in?
+      erb :"collections/create_collection"
+    else
+      redirect "/login"
+    end
+  end
+
   get '/collections/:slug' do
     @collection = Collection.find_by_slug(params[:slug])
     erb :"collections/show"
   end
+
 end
