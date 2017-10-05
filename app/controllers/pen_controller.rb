dd2c7ae7147dcca1c21d30f9406d618268fd4aca
jonathan <jonathanyi81@gmail.com>
2017-10-05 00:48:36 -0400

    added initial pen route, showing all pens for user. show.erb created also

--- app/controllers/pen_controller.rb
+++ app/controllers/pen_controller.rb
@@ -1,2 +1,13 @@
+require 'rack-flash'
 class PenController < ApplicationController
+  use Rack::Flash
+
+  get '/pens' do
+    if logged_in?
+      @pens = current_user.pens
+      erb :"pens/pens"
+    else
+      redirect "/login"
+    end
+  end
 end
