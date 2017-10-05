845044f5e936682b5e65733de0459fd228407a22
jonathan <jonathanyi81@gmail.com>
2017-10-05 19:56:18 -0400

    added get pen/:slug and view to show pen detail. Edited some other views to change the spacing of elements

--- app/controllers/pen_controller.rb
+++ app/controllers/pen_controller.rb
@@ -10,4 +10,19 @@ class PenController < ApplicationController
       redirect "/login"
     end
   end
+
+  get '/pens/:slug' do
+    if logged_in?
+      @pen = Pen.find_by_slug(params[:slug])
+      @inks = Ink.all
+      if !!@pen & current_user.pens.include?(@pen)
+        erb :"pens/show"
+      else
+        flash[:message] = "Pen #{params[:slug]} was not found."
+        redirect "/pens"
+      end
+    else
+      redirect "/login"
+    end
+  end
 end
