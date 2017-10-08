3d55e942938b18ed092575e11fe08b40f12c4b42
jonathan <jonathanyi81@gmail.com>
2017-10-08 00:54:03 -0400

    started on inks controller, inks main view created

--- app/controllers/ink_controller.rb
+++ app/controllers/ink_controller.rb
@@ -1,2 +1,13 @@
+require 'rack-flash'
 class InkController < ApplicationController
+    use Rack::Flash
+
+    get '/inks' do
+      if logged_in?
+        @inks = current_user.inks
+        erb :"inks/inks"
+      else
+        redirect "/login"
+      end
+    end
 end
