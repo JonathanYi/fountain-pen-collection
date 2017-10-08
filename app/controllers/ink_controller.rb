b3a0bc385ac90b1c957e1f28706f51514ef848b4
jonathan <jonathanyi81@gmail.com>
2017-10-08 01:24:50 -0400

    added controller routes for creating a new ink, created view as well.

--- app/controllers/ink_controller.rb
+++ app/controllers/ink_controller.rb
@@ -10,4 +10,27 @@ class InkController < ApplicationController
         redirect "/login"
       end
     end
+
+    get '/inks/new' do
+      if logged_in?
+        @inks = current_user.inks
+        @collections = current_user.collections
+        erb :"inks/create_ink"
+      else
+        redirect "/login"
+      end
+    end
+
+    post '/inks' do
+      #binding.pry
+      if params[:name] == "" || params[:brand] == ""
+        flash[:message] = "Please fiill out name and brand."
+        redirect "/inks/new"
+      else
+        @ink = Ink.create(params)
+        @ink.user_id = current_user.id
+        @ink.save
+        redirect "/inks/#{@ink.slug}"
+      end
+    end
 end
